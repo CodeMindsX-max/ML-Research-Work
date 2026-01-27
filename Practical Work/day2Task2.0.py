05c682ec40c34af53d49248084b22ff20dcb50eb
CodeMindsX-max <codemindsx@gmail.com>
2026-01-27 18:56:11 +0500

    Completed Day 2 task

---
+++ Practical Work/day2Task2.0.py
@@ -0,0 +1,142 @@
+import numpy as np
+"""
+Problem no. 2.0
+==Student Marks Management System==
+Scenario: You’re building the backend logic for a school's internal grading tool.
+
+>>Create a program that:
+
+>have sample dic with hard coded data {'Ali':[78,85,90],'Sara':[88,92,79]}
+
+>>Takes user input:
+
+>name (must be in string add validation)
+>marks (marks must be integer or float number add validation)
+>store student in dictionary their names become 'key' and value will be list of number of diff subjects
+
+>>Functions to create:
+
+>add_student(student_dict)
+*take user input
+*Adds a new student with marks
+*Prevent duplicate student names
+
+>calculate_average(marks_list)
+*return average (don't print it)
+
+>get_topper(student_dict)
+*return name + average of top student
+
+>get_failed_student(student_dict)
+*Return list of students whose average < 50
+
+--Must use functions
+--Use loops + conditions 
+--use lambda function only where it makes sense
+->now this is little advance python enjoy it
+"""
+def validate_num(n):
+    return n.isdigit()
+
+def add_student(std_dic):
+    mark=[]
+    while(True):
+        name=input('Enter you name: ').strip()
+        if name.isalpha():
+            break
+        print('Name must contain only alphabets!')
+            
+
+    if name in std_dic:
+        choice=input('Name exist, press "y" to update them or "n" to cancel:')
+        if choice.lower()!='y':
+            print('Operation cancelled')
+            return std_dic
+        
+
+    for i in range(3):
+        while(True):
+            m=input(f"Enter your marks in subject {i+1}:")
+            if validate_num(m):
+                mark.append(int(m))
+                break
+            print('Invalid number, Enter numeric number!')
+
+    std_dic[name]=mark
+    print('Student data entered successfully!!')
+    return std_dic
+
+def calculate_average(marks_list):
+    average=sum(marks_list)/len(marks_list)
+    return average
+
+"""
+Another way to calculate avg if you want to become data analyst and learning NumPy
+ def calculate_averages(marks_list):
+     arr=np.array(marks_list)
+     average=np.mean(arr)
+     return average
+"""
+
+def get_topper(std_dic):
+    avg=[]
+    name=[]
+    for k,v in std_dic.items():
+        name.append(k)
+        avg.append(calculate_average(v))
+
+    max_avg=max(avg)
+    max_avg_index=avg.index(max_avg)
+    print(f"{name[max_avg_index]} is a topper with {max_avg} average marks")
+
+
+
+
+"""
+Chatgpt style :) below and yes more advance style to write function not tradional
+def get_topper(std_dic):
+    topper = max(std_dic, key=lambda name: calculate_average(std_dic[name]))
+    avg = calculate_average(std_dic[topper])
+    print(f"{topper} is a topper with {avg} average marks")
+"""
+
+def get_falied(std_dic):
+    avg=[]
+    name=[]
+    for k,v in std_dic.item():
+        name.append(k)
+        avg.append(calculate_average(v))
+    for i in range(len(avg)):
+        if avg[i]<50:
+            print(f'name[i] failed with {avg[i]} average marks')
+
+
+def main():
+    std_dic={'Ali':[78,85,90],'Sara':[88,92,79]}
+    while True:
+        while True:
+            choce=input("Press 1 to add student\n" \
+            "Press 2 to check topper student\n" \
+            "Press 3 to check failed students\n" \
+            "Press anything else to exit\n" \
+            "Your choice: ")
+            if validate_num(choce):
+                choice=int(choce)
+                break
+            else:
+                print('Wrong input, try again')
+
+        if choice==1:
+            std_dic=add_student(std_dic)
+            continue
+        elif choice==2:
+            get_topper(std_dic)
+            continue
+        elif choice==3:
+            get_falied(std_dic)
+            continue
+        else:
+            break
+
+
+main()
