532dcb03c4977391372148d596d14c0c55080ec4
CodeMindsX-max <codemindsx@gmail.com>
2026-02-15 13:22:47 +0500

    Connection successfull with backend postgre pgAdmin

--- ML Projects/university-management-system/backend/app/main.py
+++ ML Projects/university-management-system/backend/app/main.py
@@ -1,12 +1,15 @@
 import os
 from dotenv import load_dotenv
 from fastapi import FastAPI
-from app.database.db import engine
+from app.database.db import engine, Base
+from app.models import student
 
 load_dotenv()
 
 app = FastAPI(title=os.getenv("PROJECT_NAME"))
 
+Base.metadata.create_all(bind=engine)
+
 @app.get("/")
 def root():
-    return {"message": "Database Connected Successfully"}
+    return {"message": "Student Table Ready"}
--- ML Projects/university-management-system/backend/app/models/student.py
+++ ML Projects/university-management-system/backend/app/models/student.py
@@ -1,22 +1,15 @@
 from sqlalchemy import Column, Integer, String
 from app.database.db import Base
 
+
 class Student(Base):
     __tablename__ = "students"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    email = Column(String, unique=True)
-    department = Column(String)
-    admission_year = Column(Integer)
+    name = Column(String, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    department = Column(String, nullable=False)
+    admission_year = Column(Integer, nullable=False)
 
-
-class Enrollment(Base):
-    __tablename__ = "enrollments"
-
-    id = Column(Integer, primary_key=True)
-    student_id = Column(Integer)
-    course_id = Column(Integer)
-    semester_id = Column(Integer)
-    instructor_id = Column(Integer)
-    grade = Column(String)
+    def __repr__(self):
+        return f"<Student {self.name}>"
